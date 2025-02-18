b2229945d9aacaad6bf35c7826f2e396ba4f8f70
eric_lee <1098422718@qq.com>
2025-02-18 18:24:05 +0800

    new

--- VirtualPyDevice/server.py
+++ VirtualPyDevice/server.py
@@ -25,4 +25,4 @@ async def post(request: Request):
 # 运行
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10001)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
