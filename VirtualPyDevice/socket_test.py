c9510c61e891adbf8265edb6990e89e23648e42d
HotBloodLee <60925260+HotBloodLee@users.noreply.github.com>
2025-02-23 17:00:13 +0800

    socket

--- VirtualPyDevice/socket_test.py
+++ VirtualPyDevice/socket_test.py
@@ -2,6 +2,13 @@
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 10016))
-s.send(b'hello')
-s.close()
+s.bind(('0.0.0.0', 8081))
+s.listen(15)
+while True:
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        print(data)
+    conn.close()
